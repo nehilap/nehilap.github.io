8a8fb693b0374bf67493b28bb11b28f860c6d436
Peter Nehila <peter.nehila@student.tuke.sk>
2020-04-02 17:33:55 +0200

    Responsive nav enhanced to work better on phones, minor css updates

---
+++ js/responsiveMenu.js
@@ -0,0 +1,20 @@
+let nav = document.getElementsByTagName("nav")[0];
+let menuBar = document.getElementsByClassName("menu-bar")[0];
+
+document.addEventListener("click", hideMenu);
+
+menuBar.addEventListener("click", toggleMenu);
+
+function hideMenu(event) {
+	//console.log(event.target);
+	//console.log(menuBar);
+	if(!(event.target == menuBar || event.target.parentNode == menuBar)){
+		//console.log("removed");
+		nav.classList.remove("show-menu");
+	}
+}
+
+function toggleMenu() {
+	//console.log("toggled");
+	nav.classList.toggle("show-menu");
+}
