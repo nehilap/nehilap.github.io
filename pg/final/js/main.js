03b43c1953b28006c596336375d8899b1cc9a630
Peter Nehila <peter.nehila@student.tuke.sk>
2021-12-09 21:33:14 +0100

    More functionality - basic game loop, loading files, screenshot

--- pg/final/js/main.js
+++ pg/final/js/main.js
@@ -3,6 +3,10 @@ var geometry, material, cube, cylinder, sphere;
 var pointLight, spotlight, lightTarget;
 var smallCanvas, smallScene, smallCamera, smallScenePart;
 var boundingBox, boxCurve; // box of object in small canvas
+var gameStatusNode;
+var freeCheckBox, animalCheckBox;
+var gameMode = 'free',
+    targetAnimal;
 
 var stopRotationButton;
 var currentRotation = 0.01;
@@ -10,10 +14,16 @@ var currentRotation = 0.01;
 var clock = new THREE.Clock();
 var keyboard = new THREEx.KeyboardState();
 
+
 var skyIndex = 0;
 var lightIndex = 0;
 const skyTextures = ['texture/sky.jpg', 'texture/sky_day-min.png', 'texture/sky_sunset-min.png', 'texture/cropped-starglobe-maya-render.jpg'];
 var animals = ['giraffe', 'elephant', 'kangaroo'];
+var animalNames = [
+    ['Žirafa', 'giraffe'],
+    ['Slon', 'elephant'],
+    ['Kengura', 'kangaroo']
+];
 
 var animalParts = ['head', 'body', 'legs', 'tail'];
 var animalPartIndex = 0;
@@ -35,13 +45,14 @@ function init() {
         currentAnimalParts.push(null)
     }
 
+    freeCheckBox = document.getElementById("freeCheckBox");
+    animalCheckBox = document.getElementById("animalCheckBox");
+    gameStatusNode = document.getElementById("gameStatus");
     stopRotationButton = document.getElementById("stopRotationButton");
-
     animal_builder = document.getElementById("animal_builder");
-
     smallCanvas = document.getElementById("smallCanvas");
-
     mainCanvas = document.getElementById("mainCanvas");
+
     mainCanvas.width = window.innerWidth / 100 * mainCanvasWidth
     mainCanvas.height = window.innerHeight;
 
@@ -68,16 +79,16 @@ function init() {
 
     controls.maxDistance = 50;
     //controls.enablePan = false;
-    controls.panSpeed = 0.4;
-    controls.rotateSpeed = 0.4;
+    controls.panSpeed = 0.3;
+    controls.rotateSpeed = 0.1;
     controls.maxPolarAngle = Math.PI / 2.01;
     controls.enableDamping = true;
     controls.dampingFactor = 0.5;
     controls.target.set(0, 0, 0);
 
-    toggleAnimalBuilder();
-
     makeSmallScene();
+    toggleAnimalBuilder();
+    setGameMode('free');
 }
 
 function mainRender() {
@@ -119,15 +130,13 @@ function smallRender() {
     smallCamera.aspect = width / height;
     smallCamera.updateProjectionMatrix();
 
-    const positiveYUpBottom = renderer.domElement.clientHeight - bottom;
-
     renderer.setScissor(left, top, width, height);
     renderer.setViewport(left, top, width, height);
 
-    if(smallScenePart == undefined){
+    if (smallScenePart == undefined) {
         smallScenePart = smallScene.getObjectByName("smallSceneAnimal");
     }
-    if(smallScenePart != undefined){
+    if (smallScenePart != undefined) {
         smallScenePart.rotation.y += currentRotation;
     }
 
@@ -362,7 +371,7 @@ function loadAnimalAllParts(animal, position, scale, scene) {
     );
 }
 
-function loadAnimalPartObj(animal, part, position, scale, scene, animalName){
+function loadAnimalPartObj(animal, part, position, scale, scene, animalName) {
     let mtlLoader = new THREE.MTLLoader();
     let path = './models/' + animal + '/';
     mtlLoader.setPath(path);
@@ -398,20 +407,20 @@ function loadObj(loader, animal, position, scale, scene, animalName, center) {
                 object.name = animalName;
             } else {
                 object.name = animal;
-            }       
+            }
 
             scene.add(object);
 
-            
+
             if (loadingSmall) {
                 smallScenePart = smallScene.getObjectByName("smallSceneAnimal");
                 if (center) {
                     let mesh = object.children[0];
                     let geometry = mesh.geometry;
                     geometry.computeBoundingBox();
-                    
+
                     let box = new THREE.Box3().setFromObject(mesh);
-                    box.center( mesh.position ); // this re-sets the mesh position
+                    box.center(mesh.position); // this re-sets the mesh position
                     mesh.position.multiplyScalar(-1);
 
                     fitCameraToObject(smallCamera, object, 0.4);
@@ -438,7 +447,7 @@ function toggleAnimalBuilder() {
     }
 }
 
-function setPart(){
+function setPart() {
     removePart();
     loadAnimalPartObj(animals[animalIndex], animalParts[animalPartIndex], position = {
         x: 0,
@@ -446,6 +455,8 @@ function setPart(){
         z: 0
     }, scale = 1, scene, "current" + animalParts[animalPartIndex]);
     currentAnimalParts[animalPartIndex] = animals[animalIndex] + "_" + animalParts[animalPartIndex];
+
+    checkStatusOfBuild();
 }
 
 function removePart() {
@@ -469,6 +480,7 @@ function stopRotation() {
 
 function switchAnimalPartCategory() {
     animalPartIndex = (animalPartIndex + 1) % animalParts.length;
+    animals = shuffle(animals);
     loadAnimalPartIntoSmallScene();
 }
 
@@ -527,10 +539,10 @@ function makeSmallScene() {
 
     let light = new THREE.AmbientLight(0xeeeeee, 0.35);
     smallScene.add(light);
-    
+
     light = new THREE.SpotLight(0xFFFFFF, 0.8);
-    light.position.set(10,5,10);
-    light.lookAt(0,0,0);
+    light.position.set(10, 5, 10);
+    light.lookAt(0, 0, 0);
     smallScene.add(light);
 }
 
@@ -545,7 +557,6 @@ function resizeRendererToDisplaySize(renderer) {
     return needResize;
 }
 
-var cube;
 function fitCameraToObject(camera, object, offset) {
     offset = offset || 0.5;
 
@@ -564,6 +575,7 @@ function fitCameraToObject(camera, object, offset) {
     const size = boundingBox.getSize(new THREE.Vector3());
     const center = boundingBox.getCenter(new THREE.Vector3());
     /*
+    var cube;
     const maxSize = Math.max( size.x, size.y, size.z );
     const fitHeightDistance = maxSize / ( 2 * Math.atan( Math.PI * camera.fov / 360 ) );
     const fitWidthDistance = fitHeightDistance / camera.aspect;
@@ -583,12 +595,192 @@ function fitCameraToObject(camera, object, offset) {
     */
     // console.log(size, boundingBox, center);
 
+    // newFov = newFov > 180 ? 60 / (1 / (Math.max(size.x, size.y, size.z) / 0.5)) : newFov;
     let newFov = 60 / (1 / (Math.max(size.x, size.y, size.z) / 0.5));
     camera.fov = newFov;
 
-    camera.position.set(center.x + offset, center.y +  offset, center.z +  offset);
+    let xPos = size.x / 2 > offset ? center.x + size.x / 2 : center.x + offset;
+    let yPos = size.y / 2 > offset ? boundingBox.max.y + size.y / 2 : center.y + offset;
+    let zPos = size.z / 2 > offset ? center.z + size.z / 2 : center.z + offset;
+
+    camera.position.set(xPos, yPos, zPos);
 
     camera.lookAt(center.x, center.y, center.z);
     camera.updateProjectionMatrix();
     //object.position.set(object.position.x - center.x, object.position.y - center.y,object.position.z - center.z);
+}
+
+function setGameMode(newGameMode) {
+    gameMode = newGameMode;
+    if (newGameMode === 'free') {
+        freeCheckBox.classList.remove("hidden");
+        animalCheckBox.classList.add("hidden");
+
+        gameStatusNode.textContent = "Voľné skladanie"
+    } else if (newGameMode === 'animal') {
+        freeCheckBox.classList.add("hidden");
+        animalCheckBox.classList.remove("hidden");
+
+        resetAllParts();
+
+        let shuffledAnimals;
+        if (!(targetAnimal == undefined || targetAnimal == null)) {
+            for (let i = 0; i < animalNames.length; i++) {
+                if (arraysIdentical(animalNames[i], targetAnimal)) {
+                    animalNames.splice(i, 1);
+                    break;
+                }
+            }
+            shuffledAnimals = shuffle(animalNames);
+            animalNames.push(targetAnimal);
+        } else {
+            shuffledAnimals = shuffle(animalNames);
+        }
+
+        gameStatusNode.textContent = "Postavte: " + shuffledAnimals[0][0];
+        targetAnimal = shuffledAnimals[0];
+    }
+}
+
+function shuffle(array) {
+    arrayToShuffle = [...array];
+    let currentIndex = arrayToShuffle.length,
+        randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [arrayToShuffle[currentIndex], arrayToShuffle[randomIndex]] = [
+            arrayToShuffle[randomIndex], arrayToShuffle[currentIndex]
+        ];
+    }
+
+    return arrayToShuffle;
+}
+
+function checkStatusOfBuild() {
+    if (gameMode === 'free') {
+        return;
+    }
+
+    for (let i = 0; i < currentAnimalParts.length; i++) {
+        if (currentAnimalParts[i] == null || !currentAnimalParts[i].includes(targetAnimal[1])) {
+            return;
+        }
+    }
+
+    setGameMode('free');
+    gameStatusNode.textContent = "Gratulujeme!! Úspešne ste postavili zvieratko";
+}
+
+function arraysIdentical(a, b) {
+    var i = a.length;
+    if (i != b.length) return false;
+    while (i--) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+};
+
+function resetAllParts() {
+    for (let index = 0; index < animalParts.length; index++) {
+        let currentPart = scene.getObjectByName("current" + animalParts[index]);
+
+        if (currentPart != undefined) {
+            scene.remove(currentPart);
+            currentAnimalParts[index] = null;
+        }
+    }
+}
+
+function takeScreenshot() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setScissor(0, 0, window.innerWidth, window.innerHeight);
+    renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
+
+    renderer.render(scene, camera);
+    camera.lookAt(scene.position.x, scene.position.y, scene.position.z);
+    let imgData;
+
+    try {
+        let strMime = "image/jpeg";
+        imgData = renderer.domElement.toDataURL(strMime);
+
+        download("obrazok.jpg", imgData.replace(strMime, "image/octet-stream"));
+    } catch (e) {
+        console.log(e);
+        return;
+    }
+}
+
+function saveBuild() {
+    try {
+        download("build.txt", 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(currentAnimalParts)));
+    } catch (e) {
+        console.log(e);
+        return;
+    }
+}
+
+function download(filename, data) {
+    let element = document.createElement('a');
+    element.setAttribute('href', data);
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    console.log("here");
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+function readSingleFileFromDrive(e) {
+    let file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    let reader = new FileReader();
+    reader.onload = function (e) {
+        let contents = e.target.result;
+        try {
+            currentAnimalParts = JSON.parse(contents);
+
+            resetAllParts();
+            for (let index = 0; index < currentAnimalParts.length; index++) {
+                if (currentAnimalParts[index] == null) {
+                    continue;
+                }
+                let [animal, part] = currentAnimalParts[index].split("_");
+                loadAnimalPartObj(animal, part, position = {
+                    x: 0,
+                    y: -0.01,
+                    z: 0
+                }, scale = 1, scene, "current" + part);
+            }
+        } catch (e) {
+            console.log(e);
+            return;
+        }
+    };
+    reader.readAsText(file);
+}
+
+function readBuild() {
+    let f = document.createElement('input');
+    f.style.display = 'none';
+    f.type = 'file';
+    f.name = 'file';
+    f.accept = '.txt';
+    document.body.appendChild(f);
+    f.addEventListener('change', readSingleFileFromDrive, false);
+    f.click();
+    document.body.removeChild(f);
 }
