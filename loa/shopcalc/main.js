e5b54922803fd68d4c17519c73b170583b84d30b
Peter Nehila <peter.nehila@student.tuke.sk>
2022-03-17 12:48:53 +0100

    History implemented, minor QoL

--- loa/shopcalc/main.js
+++ loa/shopcalc/main.js
@@ -1,8 +1,14 @@
 const form = document.getElementById("calculateForm");
+const historyElem = document.getElementById("historyElem");
 
 const outCCost = document.getElementById("outCCost");
 const outCValue = document.getElementById("outCValue");
 const outCrCost = document.getElementById("outCrCost");
+
+tryInitParams();
+
+let history = JSON.parse(localStorage.getItem("history"));
+setHistory(history);
 
 // ...and take over its submit event.
 form.addEventListener("submit", function (event) {
@@ -16,7 +22,67 @@ form.addEventListener("submit", function (event) {
 		iCraft: document.getElementById("iCraft").value
 	}
 
+	calculateOut(data);
+
+	if (history == null) {
+		history = [data];
+	} else {
+		history.unshift(data);
+	}
+
+	localStorage.setItem("history", JSON.stringify(history));
+	setHistory(history);
+});
+
+function setHistory(history) {
+	if (history == null) {
+		return;
+	}
+	while (historyElem.firstChild) {
+		historyElem.removeChild(historyElem.lastChild);
+	}
+
+	for (let index = 0; index < history.length; index++) {
+		const element = history[index];
+
+		var tag = document.createElement("a");
+		tag.href = "#";
+		tag.addEventListener("click", function (event) {
+			reuseData(history[index]);
+			addUrlParameters(history[index]);
+		})
+		var text = document.createTextNode(history[index].nItems + ", " + history[index].cCost + ", " + history[index].cPrice + ", " + history[index].iCost + ", " + history[index].iCraft);
+		tag.appendChild(text);
+		historyElem.appendChild(tag);
+	}
+}
+
+function reuseData(data) {
+	document.getElementById("nItems").value = data.nItems;
+	document.getElementById("cCost").value = data.cCost;
+	document.getElementById("cPrice").value = data.cPrice;
+	document.getElementById("iCost").value = data.iCost;
+	document.getElementById("iCraft").value = data.iCraft;
+
+	calculateOut(data)
+}
+
+function calculateOut(data) {
 	outCCost.innerText = (data.cPrice / 95 * data.cCost).toFixed(3);
 	outCValue.innerText = (data.nItems * data.iCost).toFixed(3);
 	outCrCost.innerText = (data.nItems * data.iCraft).toFixed(3);
-});
+}
+
+function tryInitParams() {
+	const queryString = window.location.search;
+	const params = new URLSearchParams(queryString);
+
+	if (params.get("nItems") != null) {
+		data = Object.fromEntries(params.entries());
+		reuseData(data);
+	}
+}
+
+function addUrlParameters(data) {
+	window.location.search = new URLSearchParams(data).toString()
+}
